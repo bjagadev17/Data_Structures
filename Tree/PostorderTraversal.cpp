addfdbcd655f6dcbf4e98664bbabdac806b17bf6
Biswajit Jagadev <69148426+bjagadev17@users.noreply.github.com>
2021-01-31 07:50:55 +0530

    Leetcode - Postorder Traversal of Binary Tree

--- Tree/PostorderTraversal.cpp
+++ Tree/PostorderTraversal.cpp
@@ -9,3 +9,23 @@ struct TreeNode {
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class Solution {
+public:
+    void postorder(TreeNode* root,  vector<int> &v)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+               
+        postorder(root->left,v);
+        postorder(root->right,v);
+        v.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        postorder(root,v);
+        return v;
+    }
+};
