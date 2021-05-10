b7e3159084ced7ae475e51c914717e4a130ab40f
Biswajit Jagadev <69148426+bjagadev17@users.noreply.github.com>
2021-05-10 19:25:47 +0530

    Leetcode - Merge Intervals

---
+++ Array/Merge_Interval.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(vector<int> a, vector<int> b)
+    {
+        if(a[0]==b[0])
+        {
+            return a[1]<b[1];
+        }
+        return a[0]<b[0];
+    }
+
+class Solution {
+public:
+    
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> non_overlap_interval;
+        
+        sort(intervals.begin(),intervals.end(),comp);
+        
+        int start = intervals[0][0];
+        
+        int end = intervals[0][1];
+        
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals[i][0]<=end)
+            {
+                end = max(end,intervals[i][1]);
+            }
+            else
+            {
+                vector<int> a;
+                a.push_back(start);
+                a.push_back(end);
+                
+                non_overlap_interval.push_back(a);
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        
+        vector<int> a;
+        a.push_back(start);
+        a.push_back(end);
+        
+        non_overlap_interval.push_back(a);
+        
+        return non_overlap_interval;
+    }
+};
