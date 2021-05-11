c666f69b95ab68df05cee8ca7c196398e19bb4d7
Biswajit Jagadev <69148426+bjagadev17@users.noreply.github.com>
2021-05-11 12:05:48 +0530

    Leetcode - Unique Paths

---
+++ DP/unique_path..cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int p[m][n];
+        memset(p,0,sizeof(p));
+        
+        for(int i=0;i<n;i++)
+        {
+            p[0][i]=1;
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            p[i][0]=1;
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                p[i][j]+=p[i-1][j]+p[i][j-1];
+            }
+        }
+        
+        return p[m-1][n-1];
+    }
+};
